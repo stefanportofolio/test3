25cfcfb7661f9d12a1596317d9cbedbe6f3b52e1
StefanDerian <Stefan_sippo@yahoo.com>
2015-11-02 11:19:55 +0700

    carousel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,15 +8,51 @@ $(document).ready(function(){
 
 
 
-	$('.arrow .next').click(function(){
+	$('.arrow .right').click(function(){
 
+		var image_now = $('.carousel .shown');
+		var next_image = image_now.next();
 
-
-
-
-
+		if(next_image.length == 0){
+			next_image = $('.carousel-item').first();
+		}
 
 		
+		image_now.animate({opacity:"toggle"},500,function(){
+			next_image.animate({opacity:"toggle"},500,function(){
+				image_now.removeClass('shown').addClass('hidden');
+				next_image.removeClass('hidden').addClass('shown');
+			});
+		});
+
+
+
+
+
+
+	});
+	$('.arrow .left').click(function(){
+
+		var image_now = $('.carousel .shown');
+		var next_image = image_now.next();
+
+		if(next_image.length == 0){
+			next_image = $('.carousel-item').last();
+		}
+
+		
+		image_now.animate({opacity:"toggle"},500,function(){
+			next_image.animate({opacity:"toggle"},500,function(){
+				image_now.removeClass('shown').addClass('hidden');
+				next_image.removeClass('hidden').addClass('shown');
+			});
+		});
+
+
+
+
+
+
 	});
 	$(window).resize(function(){
 		if($(window).width()<764){
